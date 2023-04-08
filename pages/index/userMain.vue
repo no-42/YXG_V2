b16dab76256244c91583d535d02923db8b4e1168
no-42 <756982469@qq.com>
2023-04-08 15:44:15 +0800

    个人中心

--- pages/index/userMain.vue
+++ pages/index/userMain.vue
@@ -2,45 +2,74 @@
 	<view>
 		<view v-if="pageValue == 0">
 			<view style="height: 50rpx; width: 50rpx;margin: 50rpx;">
-				<image src="../../static/yxg-logo.png" mode="aspectFit"
+				<image src="../../static/yxg-logo-2.png" mode="aspectFit"
 					style="border-radius: 50px;height: 100%;width: 100%;">
-			
+
 			</view>
 			<view style="margin: 50rpx;">
 				<u-text class="logo-text" size="25" :bold="true" text="上午好,领航员"></u-text>
 			</view>
 			<u-line length="10%" color="rgb(255,126,118)" margin="50rpx" :hairline="true"></u-line>
 		</view>
-		
-		
+
+
 		<view v-if="pageValue == 1">
 			<view style="margin: 50rpx;">
 				<u-text class="logo-text" size="25" :bold="true" text="消息通知"></u-text>
 			</view>
 			<u-line length="10%" color="rgb(255,126,118)" margin="30rpx" :hairline="true"></u-line>
 		</view>
-		
+
 		<view v-if="pageValue == 2">
+
 			<view style="margin: 50rpx;">
-				<u-text class="logo-text" size="25" :bold="true" text="个人中心"></u-text>
+				<u-row>
+					<u-col span="10">
+						<u-text class="logo-text" size="25" :bold="true" text="个人中心"></u-text>
+					</u-col>
+					<u-col span="2" style="height: 50rpx; width: 50rpx;">
+						<u-icon name="scan" size="30"></u-icon>
+					</u-col>
+				</u-row>
 			</view>
 			<u-line length="10%" color="rgb(255,126,118)" margin="30rpx" :hairline="true"></u-line>
-			
-			<u-avatar text="无头像"></u-avatar>
+			<view style="margin: 50rpx;">
+				<view style="margin-top: 20rpx;margin-bottom: 20rpx;height: 300rpx;">
+					<u-row style="background-image: url('../../static/login.jpg');height: 100%;border-radius: 20px;">
+						<u-col span="6">
+							<u-avatar text="无头像" size="80" mode="aspectFit" style="margin-left: 20rpx;"></u-avatar>
+						</u-col>
+						<u-col span="6">
+							<u-text size="25" :bold="true" color="white" text="用户名"></u-text>
+						</u-col>
+					</u-row>
+				</view>
+				<u-cell-group>
+					<u-cell title="服务" :isLink="true">
+						<u-icon slot="icon" size="32" name="../../static/yxg-logo-2.png"
+							style="border-radius: 50px;height: 100%;width: 100%;"></u-icon>
+
+					</u-cell>
+					<u-cell title="卡包" :isLink="true">
+						<u-icon slot="icon" size="32" name="../../static/pack.png"></u-icon>
+
+					</u-cell>
+					<u-cell title="设置" :isLink="true">
+						<u-icon slot="icon" size="32" name="../../static/setting.png"></u-icon>
+
+					</u-cell>
+
+				</u-cell-group>
+			</view>
+
 		</view>
-		
+
 		<u-tabbar :value="pageValue" @change="pageChange" :fixed="true" :placeholder="true" :safeAreaInsetBottom="true">
 			<u-tabbar-item icon="home">
-<!-- 				<image class="tabbar-item-img" slot="active-icon" src="../../static/homeActive.png"></image>
-				<image class="tabbar-item-img" slot="inactive-icon" src="../../static/homeInactive.png"></image> -->
 			</u-tabbar-item>
 			<u-tabbar-item icon="bell">
-				<!-- <image class="tabbar-item-img" slot="active-icon" src="../../static/messageActive.png"></image>
-				<image class="tabbar-item-img" slot="inactive-icon" src="../../static/messageInactive.png"></image> -->
 			</u-tabbar-item>
 			<u-tabbar-item icon="account">
-				<!-- <image class="tabbar-item-img" slot="active-icon" src="../../static/userrActive.png"></image>
-				<image class="tabbar-item-img" slot="inactive-icon" src="../../static/userInactive.png"></image> -->
 			</u-tabbar-item>
 		</u-tabbar>
 	</view>
@@ -66,5 +95,11 @@
 	.tabbar-item-img {
 		width: 40rpx;
 		height: 40rpx;
+	}
+
+	.img {
+		width: 100%;
+		height: 100%;
+		position: absolute;
 	}
 </style>
